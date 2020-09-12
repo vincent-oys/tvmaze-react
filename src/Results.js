60d66398a3944745a0bbd6cd8ef12790b8b404db
vincent-oys <vincent.ong90@gmail.com>
2020-09-12 19:25:02 +0800

    completed API call

--- src/Results.js
+++ src/Results.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
-import { results } from "./resultsData.js"
+// import { results } from "./resultsData.js"
+const axios = require("axios")
 
 export default class Results extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        let searchKeyWord = this.props.searchTitle;
+        let url = `http://api.tvmaze.com/search/shows?q=${searchKeyWord}`
+        axios.get(url)
+            .then((res) => {
+                const results = res.data;
+                this.setState({
+                    movies: results
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
     render() {
-        let { searchTitle, reset } = this.props
+        let { reset } = this.props
 
-        let movie = results.map((item, index) => {
-            if (item.show.name.toLowerCase().includes(searchTitle.toLowerCase())) {
-                return (
-                    <div>
-                        <img src={item.show.image.medium} alt="movie image" />
-                        <p>{item.show.name}</p>
-                        <p>{item.score}</p>
-                    </div>
-                )
-            }
+        let movie = this.state.movies.map((item, index) => {
+            return (
+                <div key={index}>
+                    {item.show.image ? <img src={item.show.image.medium} alt="" /> : <p>{item.show.name}'s Image Not Available</p>}
+                    <p>{item.show.name}</p>
+                    <p>{item.score}</p>
+                </div>
+            )
         })
 
         return (
@@ -23,8 +43,6 @@ export default class Results extends Component {
                 <button onClick={(e) => { reset(e) }}>Search Again</button>
                 <div>{movie}</div>
             </div>
-
         )
-
     }
 }
